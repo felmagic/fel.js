5266cd26e28814be5a48988e024d97aa579c3cf7
Mor Sela <morsela@docady.com>
2016-06-19 15:36:52 +0300

    added initial fel-text code

--- src/fel/fel-text.js
+++ src/fel/fel-text.js
@@ -1,8 +1,23 @@
 
 $(function() {
-    setTimeout(felifyText, 1000);
+    setTimeout(felifyText, 5000);
 });
 
-function felifyText(){
-  alert("felifyText!");
+function felifyText(){ 
+  console.log("felify text!");
+
+  $("span, li, a, p, h1, h2, h3, code, input").each(function(index) {
+    var obj       = $(this);
+    var text      = obj.text();
+    if (text != undefined && text != "") {
+      
+      if (Math.floor((Math.random() * 10) + 1) < 2) {
+        $(this).css('color', "rgb(151, 189, 13)")
+          .delay(index * 50)
+          .fadeOut(1500);
+      }
+    }
+  });  
+
+  setTimeout(felifyText, 5000);
 }
